e201d2048cf506a777193c3bc7f5adca49e58cd9
karinaesparza <karinaesparza296@gmail.com>
2024-03-15 20:00:31 -0300

    Criando DbContext e instalado os pacotes do EF Core

---
+++ src/Dev.Data/Context/MeuDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dev.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> db) : base(db)
+        {
+            
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+    }
+}
