93e98851fc30e31980c47195dc1253060016f1de
karinaesparza <karinaesparza296@gmail.com>
2024-03-15 20:42:12 -0300

    Config tabela Produtos

---
+++ src/Dev.Data/Mapping/ProdutoMapping.cs
@@ -0,0 +1,28 @@
+﻿
+using Dev.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dev.Data.Mapping
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(x => x.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(1000)");
+
+            builder.Property(x => x.Valor)
+                    .HasColumnType("decimal(6,2)");
+
+            builder.ToTable("Produtos");
+        }
+    }
+}
