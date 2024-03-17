b10939f7225daa583edce825d3a5b51ff76d1c29
karinaesparza <karinaesparza296@gmail.com>
2024-03-17 13:57:30 -0300

    Criando classe BaseService

---
+++ src/Dev.Business/Services/BaseService.cs
@@ -0,0 +1,45 @@
+﻿using Dev.Business.Interfaces;
+using Dev.Business.Models;
+using Dev.Business.Notification;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Dev.Business.Services
+{
+    public abstract class BaseService
+    {   
+        private readonly INotificador _notificador;
+        protected BaseService(INotificador notificador) 
+        {
+            _notificador = notificador;
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach(var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected bool ExecutarValidacao<TV, TE>(TV validator, TE entity ) 
+            where TV : AbstractValidator<TE> 
+            where TE : Entity
+        {
+            var validationResult = validator.Validate(entity);
+
+            if(validationResult.IsValid) return true;
+
+            Notificar(validationResult);
+
+            return false;
+            
+        }
+
+    }
+}
