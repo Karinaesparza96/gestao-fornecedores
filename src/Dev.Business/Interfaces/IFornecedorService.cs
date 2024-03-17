f27e5d7c81d620b53f8e40ebf53203f9b9223278
karinaesparza <karinaesparza296@gmail.com>
2024-03-17 13:16:27 -0300

    Interfaces de Serviço

---
+++ src/Dev.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,13 @@
+﻿using Dev.Business.Models;
+
+namespace Dev.Business.Interfaces
+{
+    public interface IFornecedorService : IDisposable
+    {
+        Task Adicionar(Fornecedor fornecedor);
+
+        Task Atualizar(Fornecedor fornecedor);
+
+        Task Remover(Guid id);
+    }
+}
---
+++ src/Dev.Business/Interfaces/IProdutoService.cs
@@ -0,0 +1,13 @@
+﻿using Dev.Business.Models;
+
+namespace Dev.Business.Interfaces
+{
+    public interface IProdutoService : IDisposable
+    {
+        Task Adionar(Produto produto);
+
+        Task Atulizar(Produto produto);
+
+        Task Remover(Guid id);
+    }
+}
