c4f784d5a9b5bff080170f6d59cb9f65e0dffec7
karinaesparza <karinaesparza296@gmail.com>
2024-03-17 11:33:10 -0300

    Repository Base

---
+++ src/Dev.Business/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dev.Business.Models;
+using System.Linq.Expressions;
+
+namespace Dev.Business.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : Entity, new()
+    {
+        Task Adicionar(TEntity entity);
+
+        Task<TEntity> ObterPorId(Guid id);
+
+        Task<List<TEntity>> ObterTodos();
+
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> expression);
+
+        Task Excluir(Guid id);
+
+        Task Atualizar(TEntity entity);
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ src/Dev.Data/Repositories/Repository.cs
@@ -0,0 +1,56 @@
+﻿using Dev.Business.Interfaces;
+using Dev.Business.Models;
+using Dev.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Dev.Data.Repositories
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {   
+        private readonly MeuDbContext _context;
+        protected DbSet<TEntity> DbSet { get; set; }
+        protected Repository(MeuDbContext context) 
+        { 
+            _context = context;
+            DbSet = _context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> ObterPorId(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<List<TEntity>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> expression)
+        {
+            return await DbSet.AsNoTracking().Where(expression).ToListAsync();
+        }
+        public virtual async Task Adicionar(TEntity entity)
+        {
+             DbSet.Add(entity);
+             await SaveChanges();
+        }
+
+        public virtual async Task Atualizar(TEntity entity)
+        {
+            DbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Excluir(Guid id)
+        {
+           DbSet.Remove(new TEntity { Id = id });
+           await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+          return await _context.SaveChangesAsync();
+        }
+    }
+}
