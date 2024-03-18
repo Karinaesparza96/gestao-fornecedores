f2da0dd7a0fc87e455b988ac930203b66b379868
karinaesparza <karinaesparza296@gmail.com>
2024-03-17 21:08:50 -0300

    Criado alguns metodos da controller Fornecedor

---
+++ src/Dev.Api/Controllers/FornecedorController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Dev.Api.DTOs;
+using Dev.Business.Interfaces;
+using Dev.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Dev.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedorController : MainController
+    {   
+        private readonly IFornecedorService _fornecedorService;
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+        public FornecedorController(INotificador notificador, 
+                                    IFornecedorService fornecedorService, 
+                                    IFornecedorRepository fornecedorRepository,
+                                    IMapper mapper) : base(notificador)
+        {
+            _fornecedorService = fornecedorService;
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+        }
+
+        [HttpGet("{id:guid}")]
+
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return fornecedor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel fornecedorViewModel)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            await _fornecedorService.Adicionar(_mapper.Map<Fornecedor>(fornecedorViewModel));
+
+           return CustomResponse(HttpStatusCode.Created, fornecedorViewModel);
+        }
+
+        private async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
+        {
+           return _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+    }
+}
