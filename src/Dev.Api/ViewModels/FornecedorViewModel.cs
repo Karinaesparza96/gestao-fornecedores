550747bcf503a1a6839d1a1468b03a2b0bc30aac
karina <karinaesparza296@gmail.com>
2024-06-06 17:42:35 -0300

    aceitando tipos nulos para cenarios de criação onde não tem o id ainda

--- src/Dev.Api/ViewModels/FornecedorViewModel.cs
+++ src/Dev.Api/ViewModels/FornecedorViewModel.cs
@@ -5,7 +5,7 @@ namespace Dev.Api.DTOs
 {
     public class FornecedorViewModel
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
@@ -17,7 +17,7 @@ namespace Dev.Api.DTOs
 
         public int TipoFornecedor { get; set; }
 
-        public DateTime DataCadastro { get; set; }
+        public DateTime? DataCadastro { get; set; }
 
         public bool Ativo { get; set; }
 
